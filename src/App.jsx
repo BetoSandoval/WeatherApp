a9f959af1617e0ae6deb32c5e62d25381201d4d0
BetoSandoval <alberto.sa1391@gmail.com>
2021-09-23 14:00:27 -0500

    Cree el boton Search places

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import AppContainer from "./assets/elements/AppContainer";
 import LeftElement from "./assets/elements/LeftElement";
 import RightElement from "./assets/elements/RightElement";
+import SearchButton from "./components/SearchButton";
 
 
 function App() {
   return (
     <AppContainer>
-      <LeftElement></LeftElement>
+      <LeftElement>
+        <SearchButton/>
+      </LeftElement>
       <RightElement></RightElement>
     </AppContainer>
   );
