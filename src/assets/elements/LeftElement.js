8e093fd2f7decfc09366495e2b25214b4abaaf93
BetoSandoval <alberto.sa1391@gmail.com>
2021-09-22 20:02:07 -0500

    Creación de las carpetas aseets, elements y los archivos GlobalStyles.css, LeftElement.js, RigthElement.js

---
+++ src/assets/elements/LeftElement.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const LeftElement = styled.div`
+  position: absolute;
+  width: 459px;
+  height: 1023px;
+  left: 0px;
+  top: 0px;
+
+  background: #1e213a;
+`;
+
+export default LeftElement;
---
+++ src/assets/elements/RightElement.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const RightElement = styled.div`
+  position: absolute;
+  width: 981px;
+  height: 1023px;
+  left: 459px;
+  top: 0px;
+
+  background: #100e1d;
+`;
+
+export default RightElement;
