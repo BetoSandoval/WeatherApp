a8a85e0cee8decb8b62ed6d07e18df404c18ace3
BetoSandoval <alberto.sa1391@gmail.com>
2021-12-15 20:52:02 -0600

    Eliminando componente button

--- src/Components/LeftContainer.jsx
+++ src/Components/LeftContainer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Button from "./Button";
 import { WeatherData } from "./WeatherData";
+import '../assets/styles/button.scss';
 
 export const LeftContainer = () => {
   return (
     <div>
-      <>
-        <Button/>
+        <button type="button" className="button"><span>Search for places</span></button>
         <WeatherData/>
-      </>
     </div>
   );
 };
