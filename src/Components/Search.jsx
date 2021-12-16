cd0d3a3c38d831ccb49f687022618529c1e880e1
BetoSandoval <alberto.sa1391@gmail.com>
2021-12-15 22:20:40 -0600

    lista la barra de busqueda

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../assets/styles/search.scss";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export const Search = () => {
   return (
     <>
       <div className="inpu-bar-container">
+        <AiOutlineSearch className="lens"/>
         <input type="text" className="input-bar" placeholder="Username" />
       </div>
 
